edddda2120dd349975244af461e607b24ea5da4e
patrick-spence <patrickspence27@gmail.com>
2019-07-10 22:24:15 -0400

    logging file

---
+++ Project0/src/com/project0/logging/Logging.java
@@ -0,0 +1,23 @@
+package com.project0.logging;
+
+import java.util.logging.Logger;
+import org.apache.log4j.*; 
+
+public class Logging {
+
+	private static Logger logger = Logger.getRootLogger(); 
+/*
+	public static void main(String[] args) {
+
+		allTheLogs();
+	}
+	static void allTheLogs() {
+		Log.trace("trace message");
+		Log.debug("debug message");
+		Log.info("info message");
+		Log.warn("warn message");
+		Log.error("error message");
+		Log.fatal("fatal message");
+	}
+	*/
+}
